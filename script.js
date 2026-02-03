eed20668aed4080e719487072460300060254b75
Ellensramalho <ellen_santos_bsf@hotmail.com>
2026-02-03 16:36:33 -0300

    Contagem regressiva para meu aniversário

---
+++ script.js
@@ -0,0 +1,20 @@
+import Countdown from "./countdown.js"; 
+
+const diasParaOMeuAniversario = new Countdown('24 March 2026 23:59:59 GMT-0300');
+
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+function updateCountdown() {
+  const { days, hours, minutes, seconds } = diasParaOMeuAniversario.total;
+
+  daysEl.textContent = days;
+  hoursEl.textContent = hours;
+  minutesEl.textContent = minutes;
+  secondsEl.textContent = seconds;
+}
+
+updateCountdown(); 
+setInterval(updateCountdown, 1000);
